27c7ed5afd352aa0a9022413675ccdc1d5c961b9
atinb <atinb@microsoft.com>
2015-12-22 14:53:29 +0530

    UnitTests

---
+++ src/test/java/UnitTest/WebUnitTest.java
@@ -0,0 +1,96 @@
+package UnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebUnitTest {
+
+	@Test
+	public void TestLinkModel() {
+		assertEquals(9, 9);
+	}
+
+	@Test
+	public void TestLinkMode2() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode3() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode4() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode5() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode6() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode7() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode8() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkMode9() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkModel10() {
+		assertEquals(9, 9);
+	}
+	
+	@Test
+	public void TestLinkModel11() {
+		assertEquals(9, 9);
+	}
+	@Test
+	public void TestLinkModel12() {
+		
+		assertTrue("The links on the Webpage are not working. Please check", 1>2);
+	}
+	
+	@Test
+	public void TestLinkModel3() {
+		SubmitForm();
+	}
+	
+	
+	private void SubmitForm()
+	{
+		ValidateForm();	
+	}
+	
+	private void ValidateForm()
+	{
+		CheckforValidArguments();	
+	}
+	private void CheckforValidArguments()
+	{
+		throw new IllegalArgumentException("Negavite values cannot be added to the cart");
+	}
+	
+	@Test
+	public void TestLinkModel14() {
+		assertEquals(9, 9);
+	}
+}
+	
+
+
